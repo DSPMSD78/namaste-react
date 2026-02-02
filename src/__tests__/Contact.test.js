fc17caef9e545043262f5ecb774c17fbec18ecd4
DSPMSD78 <prasad.duggirala.369@gmail.com>
2026-02-02 17:37:00 +0530

    "Episode-13/Part-2"

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -2,32 +2,34 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
+describe("Contact us Page Test cases", () => {
+  it("Should load contact us component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load button inside component", () => {
-  render(<Contact />);
-  const button = screen.getByRole("button");
-  expect(button).toBeInTheDocument();
-});
+  it("Should load button inside component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should load button inside component", () => {
-  render(<Contact />);
-  const button = screen.getByText("Submit");
-  expect(button).toBeInTheDocument();
-});
+  it("Should load button inside component", () => {
+    render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
 
-test("Should have placeholder text inside component", () => {
-  render(<Contact />);
-  const text = screen.getByPlaceholderText("name");
-  expect(text).toBeInTheDocument();
-});
+  it("Should have placeholder text inside component", () => {
+    render(<Contact />);
+    const text = screen.getByPlaceholderText("name");
+    expect(text).toBeInTheDocument();
+  });
 
-test("Should have 2 placeholder text inside component", () => {
-  render(<Contact />);
-  const inputBoxes = screen.getAllByRole("textbox");
-  expect(inputBoxes.length).toBe(2);
+  it("Should have 2 placeholder text inside component", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+  });
 });
